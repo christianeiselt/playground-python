0699ea9cdd2206d3f1729f78511477c11874675d
Christian Eiselt <dev@eiselt.ch>
2021-10-02 23:01:19 +0200

    fix(file): fix linting issue C0115
    C0115: Missing class docstring (missing-class-docstring)

--- test_unittest.py
+++ test_unittest.py
@@ -3,6 +3,8 @@
 import inc_dec    # The code to test
 import unittest   # The test framework
 
+"""Class to test increment and decrement functions."""
+
 
 class TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
