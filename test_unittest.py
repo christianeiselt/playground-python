f68dbca30cc76bf0076a9484243155a637148f27
Christian Eiselt <dev@eiselt.ch>
2021-10-02 23:00:35 +0200

    fix(file): fix linting issue C0103
    C0103: Class name "Test_TestIncrementDecrement" doesn't conform to PascalCase naming style (invalid-name)

--- test_unittest.py
+++ test_unittest.py
@@ -4,7 +4,7 @@ import inc_dec    # The code to test
 import unittest   # The test framework
 
 
-class Test_TestIncrementDecrement(unittest.TestCase):
+class TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
         self.assertEqual(inc_dec.increment(3), 4)
 
