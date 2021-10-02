76ad8821f7cf6e1747e3fbf83252247b0904a0d9
Christian Eiselt <dev@eiselt.ch>
2021-10-02 22:56:55 +0200

    fix(file): fix linting issue C0116
    C0116: Missing function or method docstring (missing-function-docstring)

--- test_pytest.py
+++ test_pytest.py
@@ -3,8 +3,10 @@ import inc_dec    # The code to test
 
 
 def test_increment():
+    """Tests incrementing for number 3."""
     assert inc_dec.increment(3) == 4
 
 
 def test_decrement():
+    """Tests decrementing for number 3."""
     assert inc_dec.decrement(3) == 2
