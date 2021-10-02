ebc488d8f1801c9dd7324f79b4f4a8c1d2931085
Christian Eiselt <dev@eiselt.ch>
2021-10-02 23:05:55 +0200

    fix(style): fix lint issues

--- test_unittest.py
+++ test_unittest.py
@@ -3,19 +3,20 @@
 import unittest   # The test framework
 import inc_dec    # The code to test
 
-"""Class to test increment and decrement functions."""
-
 
 class TestIncrementDecrement(unittest.TestCase):
+
+    """Class to test increment and decrement functions."""
+
+
+def test_increment(self):
     """Tests incrementing for number 3."""
+    self.assertEqual(inc_dec.increment(3), 4)
 
-    def test_increment(self):
-        self.assertEqual(inc_dec.increment(3), 4)
 
+def test_decrement(self):
     """Tests decrementing for number 3."""
-
-    def test_decrement(self):
-        self.assertEqual(inc_dec.decrement(3), 2)
+    self.assertEqual(inc_dec.decrement(3), 2)
 
 
 if __name__ == '__main__':
