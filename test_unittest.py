184de7443c5de72a3d9f6464739ab43c703e058c
Christian Eiselt <dev@eiselt.ch>
2021-10-02 23:02:51 +0200

    fix(file): fix linting issue C0411
    C0411: standard import "import unittest" should be placed before "import inc_dec" (wrong-import-order)

--- test_unittest.py
+++ test_unittest.py
@@ -1,7 +1,7 @@
 """Test for module inc_dec with unittest framework."""
 
-import inc_dec    # The code to test
 import unittest   # The test framework
+import inc_dec    # The code to test
 
 """Class to test increment and decrement functions."""
 
